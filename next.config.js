8ac10b53a99edb4bf1dac799b0a1c6f6a0a6cc07
Matisse <matisse.van.steenbergen@student.howest.be>
2021-12-07 03:03:55 +0100

    testing out production in middleware

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    concurrentFeatures: true,
+    serverComponents: true,
+    urlImports: [
+      'https://cdn.skypack.dev',
+      "https://framer.com/m/",
+      "https://framerusercontent.com/",
+      "https://ga.jspm.io/",
+      "https://jspm.dev/",
+    ],
+  }
 }
