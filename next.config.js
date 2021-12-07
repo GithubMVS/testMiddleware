51f17b67e497bc92a09127a4c44c600f1e39c084
Matisse <matisse.van.steenbergen@student.howest.be>
2021-12-07 10:27:20 +0100

    testing out production in middleware

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 const withTM = require('@vercel/edge-functions-ui/transpile')()
 
-module.exports = withTM()
+module.exports = withTM({
+  reactStrictMode: true,
+  experimental: {
+    concurrentFeatures: true,
+    serverComponents: true,
+    urlImports: [
+      'https://cdn.skypack.dev',
+      "https://framer.com/m/",
+      "https://framerusercontent.com/",
+      "https://ga.jspm.io/",
+      "https://jspm.dev/",
+    ],
+  }
+})
