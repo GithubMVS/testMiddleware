98de1075a0cf6fdc6af714ac28261b33a627d0a9
Matisse <matisse.van.steenbergen@student.howest.be>
2021-12-08 15:08:55 +0100

    making home page better + adding explenation for middleware + small fixes on frontend of server components

--- next.config.js
+++ next.config.js
@@ -1,17 +1,1 @@
-const withTM = require('@vercel/edge-functions-ui/transpile')()
-
-module.exports = withTM(), {
-  reactStrictMode: true,
-  experimental: {
-    concurrentFeatures: true,
-    serverComponents: true,
-    urlImports: [
-      'https://cdn.skypack.dev',
-      "https://framer.com/m/",
-      "https://framerusercontent.com/",
-      "https://ga.jspm.io/",
-      "https://jspm.dev/",
-    ],
-  }
-}
-
+module.exports = {}
