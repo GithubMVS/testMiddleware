4713abed0f92d175e2a2d03d881f9b39bba9f8f6
Matisse <matisse.van.steenbergen@student.howest.be>
2021-12-08 15:12:16 +0100

    making home page better + adding explenation for middleware + small fixes on frontend of server components

--- next.config.js
+++ next.config.js
@@ -1,1 +1,15 @@
-module.exports = {}
+module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    concurrentFeatures: true,
+    serverComponents: true,
+    urlImports: [
+      'https://cdn.skypack.dev',
+      "https://framer.com/m/",
+      "https://framerusercontent.com/",
+      "https://ga.jspm.io/",
+      "https://jspm.dev/",
+    ],
+  }
+}
+
